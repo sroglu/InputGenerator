a68d0ef8395c1b64d8cb784318a072b874db04c9
sroglu <sroglu.mehmet@gmail.com>
2022-11-16 23:33:23 +0300

    Initial commit

---
+++ DataContainer/CombinationData.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using InputGenerator.Utils;
+
+namespace InputGenerator.DataContainer
+{
+
+    public enum CombinationCommand
+    {
+        [Description("")]
+        CharacterInput,
+        [Description("Execute")]
+        Execute,
+        [Description("LeftClick")]
+        LeftClick,
+        [Description("DoubleClick")]
+        DoubleClick,
+        [Description("RightClick")]
+        RightClick,
+        [Description("%")]
+        ALT,
+        [Description("^")]
+        CTRL,
+        [Description("+")]
+        SHIFT,
+        [Description("{BACKSPACE}")]
+        BACKSPACE,
+        [Description("{BREAK}")]
+        BREAK,
+        [Description("{CAPSLOCK}")]
+        CAPS_LOCK,
+        [Description("{DELETE}")]
+        DELETE,
+        [Description("{DOWN}")]
+        DOWN_ARROW,
+        [Description("{END}")]
+        END,
+        [Description("{ENTER}")]
+        ENTER,
+        [Description("{ESC}")]
+        ESC,
+        [Description("{HELP}")]
+        HELP,
+        [Description("{HOME}")]
+        HOME,
+        [Description("{INSERT}")]
+        INSERT,
+        [Description("{LEFT}")]
+        LEFT_ARROW,
+        [Description("{LEFTNUMLOCK}")]
+        NUM_LOCK,
+        [Description("{PGDN}")]
+        PAGE_DOWN,
+        [Description("{PGUP}")]
+        PAGE_UP,
+        [Description("{PRTSC}")]
+        PRINT_SCREEN,
+        [Description("{RIGHT}")]
+        RIGHT_ARROW,
+        [Description("{SCROLLOCK}")]
+        SCROLL_LOCK,
+        [Description("{TAB}")]
+        TAB,
+        [Description("{UP}")]
+        UP_ARROW,
+        [Description("{F1}")]
+        F1,
+        [Description("{F2}")]
+        F2,
+        [Description("{F3}")]
+        F3,
+        [Description("{F4}")]
+        F4,
+        [Description("{F5}")]
+        F5,
+        [Description("{F6}")]
+        F6,
+        [Description("{F7}")]
+        F7,
+        [Description("{F8}")]
+        F8,
+        [Description("{F9}")]
+        F9,
+        [Description("{F10}")]
+        F10,
+        [Description("{F11}")]
+        F11,
+        [Description("{F12}")]
+        F12,
+        [Description("{F13}")]
+        F13,
+        [Description("{F14}")]
+        F14,
+        [Description("{F15}")]
+        F15,
+        [Description("{F16}")]
+        F16,
+        [Description("{ADD}")]
+        Keypad_Add,
+        [Description("{SUBTRACT}")]
+        Keypad_Subtract,
+        [Description("{MULTIPLY}")]
+        Keypad_Multiply,
+        [Description("{DEVIDE}")]
+        Keypad_Divide,
+        [Description("")]
+        COUNT
+    }
+
+    public class CombinationData
+    {
+        public static Dictionary<CombinationCommand, string> CombinationDictionary;
+        public static void NameInputs()
+        {
+            CombinationDictionary = new Dictionary<CombinationCommand, string>();
+            CombinationDictionary.Add(CombinationCommand.CharacterInput, "CharacterInput");
+            CombinationDictionary.Add(CombinationCommand.Execute, "Execute");
+            CombinationDictionary.Add(CombinationCommand.LeftClick, "LeftClick");
+            CombinationDictionary.Add(CombinationCommand.RightClick, "RightClick");
+            CombinationDictionary.Add(CombinationCommand.DoubleClick, "DoubleClick");
+            CombinationDictionary.Add(CombinationCommand.ALT, "ALT");
+            CombinationDictionary.Add(CombinationCommand.CTRL, "CTRL");
+            CombinationDictionary.Add(CombinationCommand.SHIFT, "SHIFT");
+            CombinationDictionary.Add(CombinationCommand.BACKSPACE, "BACKSPACE");
+            CombinationDictionary.Add(CombinationCommand.BREAK, "BREAK");
+            CombinationDictionary.Add(CombinationCommand.CAPS_LOCK, "CAPS LOCK");
+            CombinationDictionary.Add(CombinationCommand.DELETE, "DELETE");
+            CombinationDictionary.Add(CombinationCommand.DOWN_ARROW, "DOWN ARROW");
+            CombinationDictionary.Add(CombinationCommand.END, "END");
+            CombinationDictionary.Add(CombinationCommand.ENTER, "ENTER");
+            CombinationDictionary.Add(CombinationCommand.ESC, "ESC");
+            CombinationDictionary.Add(CombinationCommand.HELP, "HELP");
+            CombinationDictionary.Add(CombinationCommand.HOME, "HOME");
+            CombinationDictionary.Add(CombinationCommand.INSERT, "INSERT");
+            CombinationDictionary.Add(CombinationCommand.LEFT_ARROW, "LEFT_ARROW");
+            CombinationDictionary.Add(CombinationCommand.NUM_LOCK, "NUM LOCK");
+            CombinationDictionary.Add(CombinationCommand.PAGE_DOWN, "PAGE DOWN");
+            CombinationDictionary.Add(CombinationCommand.PAGE_UP, "PAGE UP");
+            CombinationDictionary.Add(CombinationCommand.PRINT_SCREEN, "PRINT SCREEN");
+            CombinationDictionary.Add(CombinationCommand.RIGHT_ARROW, "RIGHT ARROW");
+            CombinationDictionary.Add(CombinationCommand.SCROLL_LOCK, "SCROLL LOCK");
+            CombinationDictionary.Add(CombinationCommand.TAB, "TAB");
+            CombinationDictionary.Add(CombinationCommand.UP_ARROW, "UP ARROW");
+            CombinationDictionary.Add(CombinationCommand.F1, "F1");
+            CombinationDictionary.Add(CombinationCommand.F2, "F2");
+            CombinationDictionary.Add(CombinationCommand.F3, "F3");
+            CombinationDictionary.Add(CombinationCommand.F4, "F4");
+            CombinationDictionary.Add(CombinationCommand.F5, "F5");
+            CombinationDictionary.Add(CombinationCommand.F6, "F6");
+            CombinationDictionary.Add(CombinationCommand.F7, "F7");
+            CombinationDictionary.Add(CombinationCommand.F8, "F8");
+            CombinationDictionary.Add(CombinationCommand.F9, "F9");
+            CombinationDictionary.Add(CombinationCommand.F10, "F10");
+            CombinationDictionary.Add(CombinationCommand.F11, "F11");
+            CombinationDictionary.Add(CombinationCommand.F12, "F12");
+            CombinationDictionary.Add(CombinationCommand.F13, "F13");
+            CombinationDictionary.Add(CombinationCommand.F14, "F14");
+            CombinationDictionary.Add(CombinationCommand.F15, "F15");
+            CombinationDictionary.Add(CombinationCommand.F16, "F16");
+            CombinationDictionary.Add(CombinationCommand.Keypad_Add, "Keypad Add");
+            CombinationDictionary.Add(CombinationCommand.Keypad_Subtract, "Keypad Subtract");
+            CombinationDictionary.Add(CombinationCommand.Keypad_Multiply, "Keypad Multiply");
+            CombinationDictionary.Add(CombinationCommand.Keypad_Divide, "Keypad Divide");
+
+        }
+
+        public int index;
+        public CombinationCommand command;
+        public string character;
+
+        public string Name
+        {
+            get
+            {
+                if ((int)command == -1) command = CombinationCommand.COUNT;
+                if (command == CombinationCommand.COUNT) return "<Empty-Command>";
+                if (command == CombinationCommand.CharacterInput) return character;
+                return CombinationDictionary[command];
+            }
+        }
+
+        public CombinationData()
+        {
+            index = -1;
+            command = CombinationCommand.COUNT;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public string SendKeyValues()
+        {
+            if (command == CombinationCommand.COUNT) return null;
+            if (command == CombinationCommand.CharacterInput) return character.ToLower();
+            return EnumUtils.GetDescription(command);
+        }
+
+
+
+
+
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,411 @@
+﻿using InputGenerator.DataContainer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using System.Threading;
+using InputGenerator.Utils;
+using Xamarin.Forms.PlatformConfiguration;
+
+namespace InputGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public enum TOUCH_MASK : uint
+        {
+            TOUCH_MASK_NONE = 0x00000000,
+            TOUCH_MASK_CONTACTAREA = 0x00000001,
+            TOUCH_MASK_ORIENTATION = 0x00000002,
+            TOUCH_MASK_PRESSURE = 0x00000004
+        }
+        public enum POINTER_INPUT_TYPE : uint
+        {
+            PT_POINTER = 0x00000001,
+            PT_TOUCH = 0x00000002,
+            PT_PEN = 0x00000003,
+            PT_MOUSE = 0x00000004
+        }
+
+        public enum POINTER_FLAGS : uint
+        {
+            POINTER_FLAG_NONE = 0x00000000,
+            POINTER_FLAG_NEW = 0x00000001,
+            POINTER_FLAG_INRANGE = 0x00000002,
+            POINTER_FLAG_INCONTACT = 0x00000004,
+            POINTER_FLAG_FIRSTBUTTON = 0x00000010,
+            POINTER_FLAG_SECONDBUTTON = 0x00000020,
+            POINTER_FLAG_THIRDBUTTON = 0x00000040,
+            POINTER_FLAG_OTHERBUTTON = 0x00000080,
+            POINTER_FLAG_PRIMARY = 0x00000100,
+            POINTER_FLAG_CONFIDENCE = 0x00000200,
+            POINTER_FLAG_CANCELLED = 0x00000400,
+            POINTER_FLAG_DOWN = 0x00010000,
+            POINTER_FLAG_UPDATE = 0x00020000,
+            POINTER_FLAG_UP = 0x00040000,
+            POINTER_FLAG_WHEEL = 0x00080000,
+            POINTER_FLAG_HWHEEL = 0x00100000
+        }
+        public enum TOUCH_FEEDBACK : uint
+        {
+            TOUCH_FEEDBACK_DEFAULT = 0x1,
+            TOUCH_FEEDBACK_INDIRECT = 0x2,
+            TOUCH_FEEDBACK_NONE = 0x3
+        }
+
+        [DllImport("TouchInjectionDriver.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern bool InjectTouch(int x, int y, POINTER_INPUT_TYPE pt_input, int pressure, int orientation, int id, int rcContactTop, int rcContactBottom, int rcContactLeft, int rcContactRight, POINTER_FLAGS pointerFlags, TOUCH_MASK touchMask);
+        [DllImport("TouchInjectionDriver.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void setTouchFeedback(TOUCH_FEEDBACK fb);
+        [DllImport("TouchInjectionDriver.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void setDefaultRectSize(int size);
+        [DllImport("TouchInjectionDriver.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void setDefaultPressure(int pres);
+        [DllImport("TouchInjectionDriver.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void setDefaultOrientation(int or);
+
+        [DllImport("User32.dll")]
+        static extern Boolean MessageBeep(UInt32 beepType);
+
+        public static void GenerateTouch(int x, int y)
+        {
+            bool ret;
+            setTouchFeedback(TOUCH_FEEDBACK.TOUCH_FEEDBACK_INDIRECT);
+            ret = InjectTouch(x, y, POINTER_INPUT_TYPE.PT_TOUCH, 3200, 0, 0, x - 4, x + 4, y - 4, y + 4, POINTER_FLAGS.POINTER_FLAG_DOWN | POINTER_FLAGS.POINTER_FLAG_INCONTACT | POINTER_FLAGS.POINTER_FLAG_INRANGE, TOUCH_MASK.TOUCH_MASK_CONTACTAREA | TOUCH_MASK.TOUCH_MASK_ORIENTATION | TOUCH_MASK.TOUCH_MASK_PRESSURE);
+            if (ret)
+            {
+                ret = InjectTouch(x, y, POINTER_INPUT_TYPE.PT_TOUCH, 3200, 0, 0, x - 4, x + 4, y - 4, y + 4, POINTER_FLAGS.POINTER_FLAG_UP, TOUCH_MASK.TOUCH_MASK_CONTACTAREA | TOUCH_MASK.TOUCH_MASK_ORIENTATION | TOUCH_MASK.TOUCH_MASK_PRESSURE);
+            }
+            else
+            {
+                MessageBeep(0);
+            }
+        }
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        [DllImport("user32")]
+        public static extern int SetCursorPos(int x, int y);
+
+        #region Fields
+        private const int MOUSEEVENTF_MOVE = 0x0001; /* mouse move */
+        private const int MOUSEEVENTF_LEFTDOWN = 0x0002; /* left button down */
+        private const int MOUSEEVENTF_LEFTUP = 0x0004; /* left button up */
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x0008; /* right button down */
+        private const int MOUSEEVENTF_RIGHTUP = 0x0010; /* right button up */
+        private const int MOUSEEVENTF_MIDDLEDOWN = 0x0020; /* middle button down */
+        private const int MOUSEEVENTF_MIDDLEUP = 0x0040; /* middle button up */
+        private const int MOUSEEVENTF_XDOWN = 0x0080; /* x button down */
+        private const int MOUSEEVENTF_XUP = 0x0100; /* x button down */
+        private const int MOUSEEVENTF_WHEEL = 0x0800; /* wheel button rolled */
+        private const int MOUSEEVENTF_VIRTUALDESK = 0x4000; /* map to entire virtual desktop */
+        private const int MOUSEEVENTF_ABSOLUTE = 0x8000; /* absolute move */
+
+        private SynchronizationContext context = null;
+        #endregion
+
+        CombinationData selectedCombination;
+
+        Stopwatch countDownWatch;
+        System.Timers.Timer tickCounter;
+        static long selectedCounterTime;
+        long counterTime;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            context = SynchronizationContext.Current;
+
+            CombinationData.NameInputs();
+
+            InputDetailPanel.Visibility = Visibility.Hidden;
+            characterInputBox.Visibility = Visibility.Hidden;
+
+
+            comboBox.ItemsSource = new BindingSource(CombinationData.CombinationDictionary, null);
+            comboBox.DisplayMemberPath = "Value";
+            comboBox.SelectionChanged += ComboBox_SelectionChanged;
+
+            listBox.SelectionChanged += ListBox_SelectionChanged;
+
+            countDownWatch = new Stopwatch();
+
+            tickCounter = new System.Timers.Timer();
+            tickCounter.Elapsed += new ElapsedEventHandler(OnTimerTick);
+            tickCounter.Interval = 1;
+            tickCounter.Enabled = false;
+
+    }
+
+        private void OnTimerTick(object sender, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke(()=> {
+
+                if (counterTime < 0)
+                {
+                    countDownLabel.Content = counterTime;
+                    return;
+                }
+
+                if (counterTime < countDownWatch.ElapsedMilliseconds)
+                {
+                    countDownLabel.Content = 0;
+                    tickCounter.Enabled = false;
+                    countDownWatch.Stop();
+
+                    OnTimeTick();
+
+                    countDownWatch.Reset();
+
+                    counterTime = selectedCounterTime;
+                    countDownWatch.Start();
+                    tickCounter.Enabled = true;
+                }
+                else
+                    countDownLabel.Content = (counterTime - countDownWatch.ElapsedMilliseconds);
+
+            });
+        }
+
+        Queue<CombinationData> actions = new Queue<CombinationData>();
+        void OnTimeTick()
+        {
+
+            StringBuilder combinationCmdBuilder = new StringBuilder(100);
+            actions.Clear();
+
+            foreach (CombinationData combData in listBox.Items)
+            {
+                actions.Enqueue(combData);
+            }
+
+            while (actions.Count > 0)
+            {
+                Console.WriteLine(actions.Peek().command);
+                if(actions.Peek().command== CombinationCommand.Execute)
+                {
+                    if (combinationCmdBuilder.Length>0)
+                    {
+                        SendKeys.SendWait(combinationCmdBuilder.ToString());
+                        combinationCmdBuilder.Clear();
+                    }
+                    actions.Dequeue();
+                }
+                else if (actions.Peek().command == CombinationCommand.LeftClick ||
+                    actions.Peek().command == CombinationCommand.RightClick||
+                    actions.Peek().command == CombinationCommand.DoubleClick)
+                {
+
+                    if (combinationCmdBuilder.Length > 0)
+                    {
+                        Console.WriteLine(combinationCmdBuilder.ToString());
+
+                        SendKeys.SendWait(combinationCmdBuilder.ToString());
+                        combinationCmdBuilder.Clear();
+                    }
+
+
+                    OperateClicks(actions.Dequeue(), System.Drawing.Point.Empty);
+
+                }
+                else
+                {
+                    combinationCmdBuilder.Append(actions.Dequeue().SendKeyValues());
+                }
+            }
+
+            if (combinationCmdBuilder.Length > 0)
+            {
+                SendKeys.SendWait(combinationCmdBuilder.ToString());
+                combinationCmdBuilder.Clear();
+            }
+
+            TouchController.Touch(200, 200);
+
+            //GenerateTouch(200, 200);
+
+            //foreach (CombinationData combData in listBox.Items)
+            //{
+            //    combinationCmdBuilder.Append(combData.SendKeyValues());
+            //}
+
+            //SendKeys.SendWait(combinationCmdBuilder.ToString());
+
+        }
+
+        void OperateClicks(CombinationData combData, System.Drawing.Point p)
+        {
+            if(p!= System.Drawing.Point.Empty)
+                SetCursorPos(p.X, p.Y);
+            Thread.Sleep(100);
+            if (combData.command == CombinationCommand.LeftClick)
+            {
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            }
+            else if (combData.command == CombinationCommand.DoubleClick)
+            {
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                Thread.Sleep(100);
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            }
+            else if (combData.command == CombinationCommand.LeftClick)
+            {
+                mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
+                mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
+            }
+            else
+            {
+                //exception
+            }
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (listBox.Items.Count == 0) {
+                comboBox.SelectedItem = null;
+                return;
+            }
+
+            selectedCombination = (CombinationData)listBox.SelectedItem;
+            if (selectedCombination == null) return;
+
+            if ((CombinationCommand)comboBox.SelectedIndex == CombinationCommand.CharacterInput) characterInputBox.Text = selectedCombination.character;
+
+            if (InputDetailPanel.Visibility != Visibility.Visible)
+                InputDetailPanel.Visibility = Visibility.Visible;
+
+            UpdateItemDetailView();
+        }
+
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            selectedCombination.command = (CombinationCommand)comboBox.SelectedIndex;
+
+            listBox.Items.Refresh();
+            listBox.UpdateLayout();
+
+            UpdateItemDetailView();
+        }
+
+
+        void UpdateItemDetailView()
+        {
+            if (selectedCombination.command < 0)
+                selectedCombination.command = CombinationCommand.COUNT;
+
+            if (selectedCombination.command == CombinationCommand.COUNT)
+                comboBox.SelectedItem = null;
+            else
+                comboBox.SelectedItem = new KeyValuePair<CombinationCommand, string>(selectedCombination.command, CombinationData.CombinationDictionary[selectedCombination.command]);
+                //comboBox.SelectedItem = new KeyValuePair<CombinationCommand, string>(selectedCombination.command, selectedCombination.ToString());
+
+            comboBox.UpdateLayout();
+
+            characterInputBox.Visibility = (((CombinationCommand)comboBox.SelectedIndex) == CombinationCommand.CharacterInput) ? Visibility.Visible : Visibility.Hidden;
+            if (characterInputBox.Visibility == Visibility.Visible)
+                characterInputBox.Text = selectedCombination.character;
+        }
+
+
+        //Add Command
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            CombinationData combination = new CombinationData();
+            listBox.Items.Add(combination);
+            combination.index = listBox.Items.IndexOf(combination);
+            listBox.SelectedIndex = listBox.Items.Count - 1;
+        }
+
+        private void CharacterInputBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if ((CombinationCommand)comboBox.SelectedIndex == CombinationCommand.CharacterInput) selectedCombination.character = characterInputBox.Text;
+            UpdateItemDetailView();
+        }
+        static int lastSelectedIndex = -1;
+        private void ButtonRemove_Click(object sender, RoutedEventArgs e)
+        {
+            lastSelectedIndex = listBox.Items.IndexOf(selectedCombination);
+
+            if (listBox.Items.Count > 1)
+            {
+                if (listBox.Items.Count > lastSelectedIndex && listBox.Items[lastSelectedIndex] != null && lastSelectedIndex > 0)
+                    listBox.SelectedItem = listBox.Items.GetItemAt(lastSelectedIndex - 1);
+                else
+                    listBox.SelectedItem = listBox.Items.GetItemAt(lastSelectedIndex + 1);
+            }
+            
+            listBox.Items.Remove(listBox.Items.GetItemAt(lastSelectedIndex));
+        }
+
+        private void SetTimerBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (timeText.Text == "") return;
+            int time;
+            int.TryParse(timeText.Text,out time);
+            selectedCounterTime = time;
+            counterTime = selectedCounterTime;
+
+            if (counterTime < 0) return;
+            countDownWatch.Start();
+            tickCounter.Enabled = true;
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void StopTimerBtn_Click(object sender, RoutedEventArgs e)
+        {
+            timeText.Text = (-1).ToString();
+            SetTimerBtn_Click(sender, e);
+        }
+
+        private void OnApplicationExit(object sender, CancelEventArgs e)
+        {
+            comboBox.SelectionChanged -= ComboBox_SelectionChanged;
+
+            listBox.SelectionChanged -= ListBox_SelectionChanged;
+
+            countDownWatch.Stop();
+
+            tickCounter.Elapsed -= new ElapsedEventHandler(OnTimerTick);
+
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if(WindowState == WindowState.Minimized)
+            {
+                ShowInTaskbar = true;
+            }
+        }
+    }
+}
